886c7b96c85aabf0210bbfc6376cb6e14abb441b
baikdk <bdk0206@naver.com>
2026-08-27 15:21:44 +0900

    CI/CD 워크플로우 완성

--- infra/modules/compute/outputs.tf
+++ infra/modules/compute/outputs.tf
@@ -1,15 +1,16 @@
 output "front_public_ip" {
-  value = aws_instance.front.public_ip
+  value = aws_eip.front_eip.public_ip
 }
 
 output "front_public_dns" {
-  value = aws_instance.front.public_dns
+  value = aws_eip.front_eip.public_dns
 }
 
 output "back_public_ip" {
-  value = aws_instance.back.public_ip
+  value = aws_eip.back_eip.public_ip
 }
 
 output "back_public_dns" {
-  value = aws_instance.back.public_dns
+  value = aws_eip.back_eip.public_dns
 }
+
